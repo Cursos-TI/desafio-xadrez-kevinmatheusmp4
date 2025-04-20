3b74d8a94b2dd33e9a42e85346f3338bea2fbb30
kevinmatheusmp4 <kevinmatheusquaresma@gmail.com>
2025-04-20 14:39:18 +0000

    Desafio MESTRE concluído

--- XadrezDesafio.c
+++ XadrezDesafio.c
@@ -1,47 +1,90 @@
 #include <stdio.h>
 
-int main () {
 
 // Movimentação da Torre
-
-for (int i = 0; i < 5; i++)
-{
-    printf("TORRE move %d casas para a direita\n", i + 1);
+void movimentoTorre(int casas) {
+    for (int i = 0; i < casas; i++) {
+        printf("TORRE move %d casas para a direita\n", i + 1);
+    }
 }
 
 //MOVIMENTAÇÃO DA RAINHA
-
-for (int i = 0; i < 8; i++)
-{
-    printf("RAINHA move %d casas para a esquerda\n", i + 1);
+void movimentoRainha(int casas) {
+    for (int i = 0; i < casas; i++) {
+        printf("RAINHA move %d casas para a esquerda\n", i + 1);
+    }
 }
 
 // MOVIMENTAÇÃO DO BISPO
-
-for (int i = 0; i < 5; i++)
-{
-    printf("BISPO move %d casas para (cima,direita)\n", i + 1);
+void movimentoBispo(int casas, char diagonal[20]) {
+    for (int i = 0; i < casas; i++) {
+        printf("BISPO move %d casas para diagonal %s\n", i + 1, diagonal);
+    }
 }
 
 // MOVIMENTAÇÃO DO CAVALO
-int movimentocavalo = 1;
-while (movimentocavalo <= 1) {
+void movimentoCavalo(int casas) {
+
+    int movimentocavalo = 1;
+    while (movimentocavalo <= 1) {
     printf("CAVALO move 2 casas para cima\n");
     for (int j = 0; j < 1; j++) {
         printf("e 1 casa para direita\n");
     }
     break; // resolve o problema de loop infinito
+    }
 }
 
-/*
+int main () {
+    int casas;
 
-GUIA DE CASAS A MOVER - NIVEL NOVATO
+    printf("BEM VINDO AO JOGO DE XADREZ\n");
+    printf("Neste jogo possuímos as seguinte peças:\n");
+    printf("1 - Torre\n");
+    printf("2 - Rainha\n");
+    printf("3 - Bispo\n");
+    printf("4 - Cavalo\n");
+    printf("5 - Sair\n");
+    printf("Escolha uma peça para movimentar: ");
+    scanf("%d", &casas);
 
-Torre move 5 - horizontal e vertical
-Bispo move 5 - diagonal (horizontal e vertical)
-Rainha move 8 - horizontal, vertical e diagonal
-
-*/
+    switch (casas) {
+        case 1:
+            printf("Quantas casas deseja mover a Torre? ");
+            scanf("%d", &casas);
+            movimentoTorre(casas);
+            break;
+        case 2:
+            printf("Quantas casas deseja mover a Rainha? ");
+            scanf("%d", &casas);
+            movimentoRainha(casas);
+            break;
+        case 3:
+            printf("Quantas casas deseja mover o Bispo? ");
+            scanf("%d", &casas);
+            printf("E para qual diagonal? (cimaDireita/cimaEsquerda/baixoDireita/baixoEsquerda): ");
+            char diagonal[20];
+            scanf("%s", diagonal);
+            movimentoBispo(casas, diagonal);
+            break;
+        case 4:
+            movimentoCavalo(casas);
+            break;
+        case 5:
+            printf("Saindo do jogo...\n");
+            return 0;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+    }
+    printf("Deseja continuar jogando? (1 - Sim, 0 - Não): ");
+    int continuar;
+    scanf("%d", &continuar);
+    if (continuar == 1) {
+        main(); // Chama a função main novamente para reiniciar o jogo
+    } else {
+        printf("Obrigado por jogar!\n");
+    }
+    
 
 return 0;
 
